a4836b91701a03155e4f9bb4bed32c0b147b4ffe
Daniel Kastl <daniel@georepublic.de>
2021-11-04 15:40:37 +0900

    Adds permissions, fixes #1
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- lib/redmine_chatwoot/view_hooks.rb
+++ lib/redmine_chatwoot/view_hooks.rb
@@ -1,19 +1,20 @@
 module RedmineChatwoot
   class ViewHooks < Redmine::Hook::ViewListener
 
-    render_on :view_layouts_base_html_head, inline: <<-END
-        <%= javascript_include_tag 'chatwoot', plugin: 'redmine_chatwoot' %>
-    END
-
     def view_layouts_base_body_bottom(context={})
-      tags = [];
-      tags.push(tag.div :data => {
-        :server => Setting.plugin_redmine_chatwoot['chatwoot_server'],
-        :token => Setting.plugin_redmine_chatwoot['chatwoot_token'],
-        :type => Setting.plugin_redmine_chatwoot['chatwoot_type'],
-        :position => Setting.plugin_redmine_chatwoot['chatwoot_position']
-      }, :id => 'chatwoot-config', :style => 'display:none')
-      return tags.join("\n")
+      if User.current.allowed_to?(:view_chatwoot, nil, :global => true)
+        tags = [];
+
+        tags << javascript_include_tag('chatwoot', :plugin => 'redmine_chatwoot')
+
+        tags.push(tag.div :data => {
+          :server => Setting.plugin_redmine_chatwoot['chatwoot_server'],
+          :token => Setting.plugin_redmine_chatwoot['chatwoot_token'],
+          :type => Setting.plugin_redmine_chatwoot['chatwoot_type'],
+          :position => Setting.plugin_redmine_chatwoot['chatwoot_position']
+        }, :id => 'chatwoot-config', :style => 'display:none')
+        return tags.join("\n")
+      end
     end
 
   end
